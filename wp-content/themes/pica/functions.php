08fc070a38678b9971cbf7304fe7fc3f83847cc8
Chloe <chloe@picadesign.com>
2011-12-29 12:54:28 -0500

    fixed font embedding

--- wp-content/themes/pica/functions.php
+++ wp-content/themes/pica/functions.php
@@ -4,7 +4,7 @@
 
 	// This theme styles the visual editor with editor-style.css to match the theme style.
 	add_editor_style();
-	
+
 	// This theme uses wp_nav_menu() in one location.
 	register_nav_menus( array(
 		'primary' => __( 'Masthead Navigation', 'FreeLandInMaine' ),
