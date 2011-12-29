931dbd7e3b39d071ca7bf37736bdbf22ced6139f
Chloe <chloe@picadesign.com>
2011-12-29 10:47:55 -0500

    Installed Wordpress and moved previous files to theme folder /pica

---
+++ wp-content/themes/pica/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Free Land In Maine</title>
+        <link rel="stylesheet" href="<?php echo get_bloginfo('template_directory');?>/style.css" />
+        <?php wp_head() ?>
+    </head>
+    
+    <body>
+    	<div id="Site-Wrapper">
+        	<div id="Masthead">
+            </div><!-- END MASTHEAD -->
