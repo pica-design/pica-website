09c3fb1779022b6180e6c413a1ce71d478454abe
Chloe <chloe@picadesign.com>
2011-12-29 11:09:17 -0500

    Installed theme 'pica', set permalinks, set mainpage to staic page: 'portfolio', disabled admin bar, created page 'page-portfolio.php'.

---
+++ wp-content/themes/pica/page-portfolio.php
@@ -0,0 +1,17 @@
+<?php get_header(); ?>
+                   <div id="contentwrapper">
+                    	<h1>Page Title</h1>
+                        <hr>
+                        	<div id="portfoliowrapper">
+                            		<div class="contentblock"></div>
+                                    <div class="contentblock"></div>
+                                    <div class="contentblock"></div>
+                                    <div class="contentblock"></div>
+                                    <div class="contentblock"></div>
+                                    <div class="contentblock"></div>
+                                    <div class="contentblock"></div>
+                                    <div class="contentblock"></div>
+                                    <div class="contentblock"></div>
+                            </div><!--end portfoliowrapper-->
+                    </div><!--end contentwrapper--> 
+<?php get_footer(); ?>
