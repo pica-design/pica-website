6624df775fa247c5ec13e7bb5a55907356db5068
James Mehorter <jamesmehorter@gmail.com>
2013-11-07 10:45:39 -0500

    Removed .DS_Store and Thumbs.db from the repo. Also removed the Approval, Success, and Holiday directories. We'll create them as individual repos and load them as submodules.

--- approval/backup/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-	//Include our MySQL Functions library
-	include("tech/gateway.php");
-	
-	//Force directory listing redirect to manage where the user will be prompted to enter a password
-	//header("Location: $manage_path");
-?>
