41040febc828c5b1545957f9786f328678b829a6
James Mehorter <james@pica.is>
2014-01-21 10:04:29 -0500

    Made page.php output the_content()

--- wp-content/themes/pica/page.php
+++ wp-content/themes/pica/page.php
@@ -1,9 +1,16 @@
-<?php get_header(); ?>
+<?php 
+	get_header(); 
+	the_post();
+?>
 	
 	<section class="sub-content-wrapper">  
         <div class="page-masthead">
-            <h1><?php echo $post->post_title ?></h1>
+            <h1><?php the_title() ?></h1>
         </div>
     </section>
+    <section class="sub-content-wrapper">
+    	<?php the_content() ?>
+    </section>
+
 
 <?php get_footer(); ?>
